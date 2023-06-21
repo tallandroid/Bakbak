cd82e90a7d8ffc69c19da27bac74de65ad8a08db
Harshit Rohatgi <harsh.rohatgi@gmail.com>
2023-06-21 22:15:02 +0530

    1. Added a test nb for live poc. 2. Added implementation of docs_scan_accessor with chroma with local dataset 3. Added test_dataset containing FAQs for flight.

--- app/docs_scan_accessor.py
+++ app/docs_scan_accessor.py
@@ -1,11 +1,24 @@
-from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
-class DocsScanAccessor:
-    def __init__(self): 
-        self.vectorstore = None
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
 
-    def retrieve_scan_docs(self):
-        return None
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+
+class DocsScanAccessor:
+    def __init__(self):
+        jq_schema = '.[]'
+        data = JSONLoader(file_path="./test_dataset.txt", jq_schema=jq_schema, text_content=False)
+        self.db = Chroma.from_documents(data.load(), huggingface_embeddings)
+        self.huggingface_embeddings = HuggingFaceEmbeddings(
+            model_name=model_name, 
+            model_kwargs=model_kwargs, 
+            encode_kwargs=encode_kwargs)
+
+    def retrieve_scan_docs(self, userInput):
+        user_input_embed = self._get_embeddings_from_inputs(userInput)
+        return self.db.similarity_search_by_vector(user_input_embed, top_k=5)
 
     def _get_embeddings_from_inputs(self, userInput):
-        return None
+        return self.huggingface.embed_query(userInput)
