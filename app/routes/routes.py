d8d9deaf854005b0d1b918763c35be8265a814ba
Harshit Rohatgi <harsh.rohatgi@gmail.com>
2023-06-23 17:41:23 +0530

    V3: Flask works. Unable to test end to end since the keys have expired. One endpoint taking just user_input for now. Moved routes to main.py.

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -7,10 +7,10 @@ prompt_generator = PromptGenerator()
 docs_accessor = DocsScanAccessor()
 response_generator = ResponseGenerator()
 
-@app.route('/get_response', methods=['GET'])
+@app.route('/response', methods=['GET'])
 def get_response():
     user_input = request.json['user_input']
-    tenant_id = request.json['tenantId']
+    #tenant_id = request.json['tenantId']
     docs = docs_accessor.retrieve_scan_docs(user_input)
     prompt = prompt_generator.generate_prompt(user_input, docs)
     return response_generator.get_generated_response(prompt)
--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from app import app
+from flask import request, Flask
+from app.docs.docs_scan_accessor import DocsScanAccessor
+from app.prompts.prompt_generator import PromptGenerator
+from app.response_generator import ResponseGenerator
+
+prompt_generator = PromptGenerator()
+docs_accessor = DocsScanAccessor()
+response_generator = ResponseGenerator()
+
+app = Flask(__name__)
+
+@app.route('/response', methods=['GET'])
+def get_response():
+    user_input = request.args.get('user_input')
+    #tenant_id = request.json['tenantId']
+    docs = docs_accessor.retrieve_scan_docs(user_input)
+    prompt = prompt_generator.generate_prompt(user_input, docs)
+    return response_generator.get_generated_response(prompt)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
